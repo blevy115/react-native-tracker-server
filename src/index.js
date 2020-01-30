b3ab7b7dfd580193e4c8c05bd196172171b87c6d
Brandon Levy <brandon.levy@mail.mcgill.ca>
2020-01-30 10:16:58 -0500

    Made initial express server and linked up to mongodb

---
+++ src/index.js
@@ -0,0 +1,33 @@
+require('./models/User');
+const express = require('express');
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes');
+
+const app = express();
+
+app.use(bodyParser.json()); // must be parsed before request handler
+app.use(authRoutes);
+
+const mongoUri = 'mongodb+srv://blevy:Linklink115@cluster0-aotky.mongodb.net/test?retryWrites=true&w=majority'
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on('connected', () => {
+  console.log('Connected to mongo instance');
+})
+
+mongoose.connection.on('error', (err) => {
+  console.log('Error connecting to mongo', err);
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello There');
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+})
