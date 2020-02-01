2b55145b2f3aa9c37d045064b95c5ca8f854b20b
Brandon Levy <brandon.levy@mail.mcgill.ca>
2020-02-01 17:07:55 -0500

    added bcrypt for salting and hashing and sigin route

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -18,4 +18,22 @@ router.post('/signup', async (req, res) => {
 
 });
 
+router.post('/signin', async (req, res) => {
+  const { email, password } = req.body
+  if(!email || !password) {
+    return res.status(422).send({error: 'Must Provide Email and Password'})
+  }
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(404).send({error: 'Email not found'})
+  }
+  try {
+    await user.comparePassword(password);
+    const token = jwt.sign({userId: user._id}, 'MY_SECRET_KEY')
+    res.send({ token });
+  } catch (err) {
+    return res.status(401).send({error: 'Invalid Password or Email'})
+  }
+})
+
 module.exports = router;
